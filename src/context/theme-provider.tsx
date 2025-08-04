3c7ce0e1c82b75ca85b9f4ffcd4fe3c57dee6f9f
Md. Ashraful Alam <md.ashraful.alam@portonics.com>
2025-08-04 11:50:58 +0000

    Only dark theme is applied real time others not.

--- src/context/theme-provider.tsx
+++ src/context/theme-provider.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -5,5 +6,10 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 import type { ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider 
+            {...props}
+            themes={['light', 'dark', 'system', 'theme-indigo', 'theme-green-orange']}
+         >
+            {children}
+         </NextThemesProvider>
 }
