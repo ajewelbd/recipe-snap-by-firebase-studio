37b9e0d4ee723ea459a37251a5d7747b229f21bf
Md. Ashraful Alam <md.ashraful.alam@portonics.com>
2025-08-04 10:54:12 +0000

    Add multiple theme: Current one, dark one, new one is according to follw

--- src/components/chefmate/header.tsx
+++ src/components/chefmate/header.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import Link from 'next/link';
-import { Globe, History, LogOut, Moon, Sun } from 'lucide-react';
+import { Globe, History, LogOut, Moon, Sun, Palette } from 'lucide-react';
 import { useTheme } from "next-themes";
 import {
   DropdownMenu,
@@ -99,11 +99,23 @@ export default function Header() {
             </DropdownMenuContent>
           </DropdownMenu>
 
-           <Button variant="outline" size="icon" onClick={() => setTheme(theme === "dark" ? "light" : "dark")} className="relative w-9 h-9">
-              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-              <span className="sr-only">Toggle theme</span>
-            </Button>
+           <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+               <Button variant="outline" size="icon" className="relative w-9 h-9">
+                  <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                  <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                  <span className="sr-only">Toggle theme</span>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
+                    <DropdownMenuRadioItem value="light">Light</DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="dark">Dark</DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="system">System</DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="theme-indigo">Indigo</DropdownMenuRadioItem>
+                </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
           
           <UserMenu />
         </div>
