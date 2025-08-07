3d17cf5366aaa039450d4c18ab028093e41ad213
Md. Ashraful Alam <md.ashraful.alam@portonics.com>
2025-08-07 10:45:10 +0000

    "Save Recipe" button not disabled while user click on it.

--- src/components/chefmate/new-recipe-form.tsx
+++ src/components/chefmate/new-recipe-form.tsx
@@ -13,16 +13,6 @@ import { Loader2 } from 'lucide-react';
 import ImageUploader from './image-uploader';
 import { Alert, AlertDescription, AlertTitle } from '../ui/alert';
 
-function SubmitButton() {
-  const { pending } = useFormStatus();
-  return (
-    <Button type="submit" disabled={pending} className="w-full">
-      {pending && <Loader2 className="mr-2 animate-spin" />}
-      Save Recipe
-    </Button>
-  );
-}
-
 export default function NewRecipeForm() {
   const initialState: FormState = { message: '', errors: {} };
   const [state, dispatch] = useActionState(saveRecipe, initialState);
@@ -33,6 +23,16 @@ export default function NewRecipeForm() {
       router.push('/my-recipes/new/success');
     }
   }, [state, router]);
+
+  function SubmitButton() {
+    const { pending } = useFormStatus();
+    return (
+      <Button type="submit" disabled={pending} className="w-full">
+        {pending && <Loader2 className="mr-2 animate-spin" />}
+        Save Recipe
+      </Button>
+    );
+  }
 
   return (
     <form action={dispatch} className="space-y-8">
